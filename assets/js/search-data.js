ac56c41e57364d623c1fed0556ab565459dc10d9
dkswarm <44557206+dkswarm@users.noreply.github.com>
2026-03-21 21:53:36 +0000

    Deploying to gh-pages from @ dkswarm/dkswarm.github.io@f4856057673d61353157f5acb929cb6d456fd4cb 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,6 +16,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
+        },{id: "nav-research",
+          title: "research",
+          description: "High-energy Astrophysics: Observation and Technology Development",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/research/";
+          },
         },{id: "nav-cv",
           title: "CV",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
