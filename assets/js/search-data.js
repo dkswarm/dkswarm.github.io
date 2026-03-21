1fa6a1e3a84a47673603fe90efcef7cbf882a90d
dkswarm <44557206+dkswarm@users.noreply.github.com>
2026-03-21 21:52:16 +0000

    Deploying to gh-pages from @ dkswarm/dkswarm.github.io@51ab4f5801c3b505ac7838aa64b145d2797522e9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,13 +16,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-research",
-          title: "research",
-          description: "A growing collection of your projects.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/research/";
-          },
         },{id: "nav-cv",
           title: "CV",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
